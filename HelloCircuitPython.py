fd792dd164917b459737bfa08b4c57dc6c869282
Andrew Dicesare <adicesa05@ccs.local>
2021-09-13 10:45:20 -0400

    edited HelloCircuitPython.py and added Servo.py

--- HelloCircuitPython.py
+++ HelloCircuitPython.py
@@ -4,10 +4,11 @@ import time
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
-print("Make it wokr!")
+print("Make it work!")
 
 while True:
     dot.fill((0,255,0))
     time.sleep(.5)
     dot.fill((255,0,0))
     time.sleep(.5)
+    dot.fill((0,0,255))
